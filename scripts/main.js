dc44abcc08b6b57d8b6488010210a33e9538b60f
Justin Sandahl <sandahl.j.d@gmail.com>
2020-08-01 13:12:40 +0100

    Implemented the logic to fetch the content data from the data.json file using the vanilla JS fetch() method to retrieve the content and then pass it down to be inserted into the elements being created dynamically. This allows the potentinal to render multiple components on the page containing different content, by adding additional sections in the data.json file, note - each new component is rendered to the top of the DOM, thus the data.json renders bottom to top if there is data for more than one component.

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,45 @@
-const mainContainer = document.querySelector(".main-container");
+fetch("content/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const mainContainer = document.querySelector(".main-container");
 
-//Create elements for the text-section of the component
-const textWrapper = document.createElement("div");
-textWrapper.classList.add("text-wrapper");
+    data.forEach(({ title, mainText, buttonText, buttonLink, imageURL }) => {
+      //Create elements for the text-section of the component
+      const textWrapper = document.createElement("div");
+      textWrapper.classList.add("text-wrapper");
 
-const heading = document.createElement("h1");
-heading.classList.add("heading");
-heading.innerText = "We're here to help with your legal services, when you need us."
+      const heading = document.createElement("h1");
+      heading.classList.add("heading");
+      heading.innerText = title;
 
-const text = document.createElement("p");
-text.classList.add("text");
-text.innerText = "For the bigger matters, and the small, we offer you a range of legal services that are built around what you need. Simple, straightforward, human and fair."
+      const text = document.createElement("p");
+      text.classList.add("text");
+      text.innerText = mainText;
 
-const button = document.createElement("button");
-button.classList.add("btn");
-button.innerText = "Popular services";
-button.addEventListener("click", () => {
-    window.location = "http://www.example.com";
-});
+      const button = document.createElement("button");
+      button.classList.add("btn");
+      button.innerText = buttonText;
+      button.addEventListener("click", () => {
+        window.location = buttonLink;
+      });
 
-textWrapper.appendChild(heading);
-textWrapper.appendChild(text);
-textWrapper.appendChild(button);
+      textWrapper.appendChild(heading);
+      textWrapper.appendChild(text);
+      textWrapper.appendChild(button);
 
-//Create elements for the image-section of the component
-const imageWrapper = document.createElement("div");
-imageWrapper.classList.add("image-wrapper");
+      //Create elements for the image-section of the component
+      const imageWrapper = document.createElement("div");
+      imageWrapper.classList.add("image-wrapper");
 
-const image = document.createElement("img");
-image.classList.add("hero-image");
-image.src = "https://sguk-uks-mkt-web-prod-02-cdn-endpoint-assets.azureedge.net/public/images/bitmap/large/157417321.jpg?version=20200720.1";
+      const image = document.createElement("img");
+      image.classList.add("hero-image");
+      image.src = imageURL;
 
-imageWrapper.appendChild(image);
+      imageWrapper.appendChild(image);
 
-/* Render textWrapper as the first child of the mainContainer 
-   and have the image section rendered directly after it */
-mainContainer.insertBefore(textWrapper, mainContainer.childNodes[0]);
-textWrapper.insertAdjacentElement("afterend", imageWrapper);
+      // Render textWrapper as the first child of the mainContainer
+      // with the imageWrapper rendered directly after it
+      mainContainer.insertBefore(textWrapper, mainContainer.childNodes[0]);
+      textWrapper.insertAdjacentElement("afterend", imageWrapper);
+    });
+  });
