3a8ad4cca8ac73cef27788b218c2bec8d664c5f2
Justin Sandahl <sandahl.j.d@gmail.com>
2020-08-01 12:32:13 +0100

    implemented the dynamic rendering of the HTML for the text-wrapper section, this whole section now uses JavaScript and the DOM API to dynamically create the text-wrapper div and its child elements while preserving  class names and event listeners.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,26 @@
-const button = document.querySelector(".btn");
+const mainContainer = document.querySelector(".main-container");
+
+const textWrapper = document.createElement("div");
+textWrapper.classList.add("text-wrapper");
+
+const heading = document.createElement("h1");
+heading.classList.add("heading");
+heading.innerText = "We're here to help with your legal services, when you need us."
+
+const text = document.createElement("p");
+text.classList.add("text");
+text.innerText = "For the bigger matters, and the small, we offer you a range of legal services that are built around what you need. Simple, straightforward, human and fair."
+
+const button = document.createElement("button");
+button.classList.add("btn");
+button.innerText = "Popular services";
 button.addEventListener("click", () => {
     window.location = "http://www.example.com";
 });
+
+textWrapper.appendChild(heading);
+textWrapper.appendChild(text);
+textWrapper.appendChild(button);
+
+//make textWrapper the first child of the mainContainer 
+mainContainer.insertBefore(textWrapper, mainContainer.childNodes[0]);
